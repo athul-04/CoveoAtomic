1a2d64a6a43e1d2c2d7b25c0712e5b313d3e2e3d
Athul Robert <athul.robert@perficient.com>
2024-12-12 18:45:33 +0530

    Updated Env

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ export const App = () => {
 
   return (
     <AtomicSearchInterface engine={atomicEngine}>
-        <SearchSection />
+          <SearchSection />
         <div className="facet-result-container">
           <div className="facet">
             <FacetSection />
